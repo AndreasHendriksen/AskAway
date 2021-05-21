c8e5860366b6924cd513ef6771cdb9cc2bcb2d34
AndreasHendriksen <70519039+AndreasHendriksen@users.noreply.github.com>
2021-05-21 10:26:10 +0200

    Removed ProfileSideBar

--- Client/askawayapp/src/Components/Layout/MainHeader.js
+++ Client/askawayapp/src/Components/Layout/MainHeader.js
@@ -2,18 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 //Components
-import ProfileSideBar from "../ProfileSideBar";
+// import ProfileSideBar from "../ProfileSideBar";
 
 //Icons
-import { IoPersonCircle } from "react-icons/io5";
+// import { IoPersonCircle } from "react-icons/io5";
 
 export default function MainHeader() {
-    const openProfile = () => {
-        document.getElementById("profileSideBar").style.transform = "translate(0, 0)";
-        document.getElementById("profileSideBar").style.boxShadow = "0 0 20px 5px rgba(0,0,0,0.1), 0 0 100px 20px rgba(0,0,0,0.2)";
-        // document.getElementById("profileSideBarBackdrop").style.opacity = "0";
-        document.getElementById("profileSideBarBackdrop").style.display = "block";
-    };
+    // const openProfile = () => {
+    //     document.getElementById("profileSideBar").style.transform = "translate(0, 0)";
+    //     document.getElementById("profileSideBar").style.boxShadow = "0 0 20px 5px rgba(0,0,0,0.1), 0 0 100px 20px rgba(0,0,0,0.2)";
+    //     // document.getElementById("profileSideBarBackdrop").style.opacity = "0";
+    //     document.getElementById("profileSideBarBackdrop").style.display = "block";
+    // };
 
     return (
         <>
@@ -22,14 +22,14 @@ export default function MainHeader() {
                 className="bg-white p-1"
                 style={{ height: 58, position: "fixed", top: 0, left: 0, right: 0, boxShadow: "0px 0px 15px 2px rgba(0,0,0,0.1)", zIndex: 100, color: "#555" }}
             >
-                <IoPersonCircle style={{ fontSize: 35, position: "absolute", top: "50%", transform: "translate(10px, -50%)" }} onClick={openProfile} />
+                {/* <IoPersonCircle style={{ fontSize: 35, position: "absolute", top: "50%", transform: "translate(10px, -50%)" }} onClick={openProfile} /> */}
                 <Link to="/" className="text-decoration-none">
                     <h1 className="text-center" style={{ fontSize: 35 }}>
                         AskAway
                     </h1>
                 </Link>
             </header>
-            <ProfileSideBar />
+            {/* <ProfileSideBar /> */}
         </>
     );
 }
--- Client/askawayapp/src/Components/Pages/HomePage.js
+++ Client/askawayapp/src/Components/Pages/HomePage.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function HomePage() {
     return (
-        <div>
-            HomePage
+        <div className="text-center" style={{position:"absolute", right:"0", bottom:"90px", left:"0"}} >
+            <button className="btn btn-primary w-75  mb-2" style={{boxShadow:"0 4px 9px 2px rgba(0,0,0,0.4)"}} >Log in</button>
+            <button className="btn btn-success w-75" style={{boxShadow:"0 4px 9px 2px rgba(0,0,0,0.4)"}} >Sign up</button>
         </div>
-    )
+    );
 }
